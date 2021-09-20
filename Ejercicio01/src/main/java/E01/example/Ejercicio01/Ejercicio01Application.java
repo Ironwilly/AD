36ceac6be6bd0249592c80b7108e62dbc688584c
Guillermo <claseswilly@gmail.com>
2021-09-20 22:31:26 +0200

    Casa 20 septiembre 2021

---
+++ Ejercicio01/src/main/java/E01/example/Ejercicio01/Ejercicio01Application.java
@@ -0,0 +1,26 @@
+package E01.example.Ejercicio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Ejercicio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio01Application.class, args);
+
+
+
+
+
+
+
+
+	}
+
+}
