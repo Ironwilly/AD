43711c41221e2c5c7fc943b4e82f80c691a7272c
Ironwilly <claseswilly@gmail.com>
2022-03-07 14:17:47 +0100

    Clase 7 marzo 2022

--- Miarma/src/main/java/com/salesianos/triana/Miarma/config/StorageProperties.java
+++ Miarma/src/main/java/com/salesianos/triana/Miarma/config/StorageProperties.java
@@ -14,5 +14,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class StorageProperties {
 
     private String location;
+
+
 }
 
--- Miarma/src/main/java/com/salesianos/triana/Miarma/security/dto/LoginDto.java
+++ Miarma/src/main/java/com/salesianos/triana/Miarma/security/dto/LoginDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@CrossOrigin(origins= "*")
 public class LoginDto {
 
     private String email;
